5b10e8911163046c8fe627b0ef7f4a727a723adb
Your Name <you@example.com>
2017-04-05 20:04:36 +0300

    add dataset downloader

---
+++ week12/lfw_dataset.py
@@ -0,0 +1,59 @@
+import numpy as np
+import os
+from scipy.misc import imread,imresize
+import pandas as pd
+
+def fetch_lfw_dataset(attrs_name = "lfw_attributes.txt",
+                      images_name = "lfw-deepfunneled",
+                      dx=80,dy=80,
+                      dimx=45,dimy=45
+    ):#sad smile
+
+    #download if not exists
+    if not os.path.exists(images_name):
+        print("images not found, donwloading...")
+        os.system("wget http://vis-www.cs.umass.edu/lfw/lfw-deepfunneled.tgz -O tmp.tgz")
+        print("extracting...")
+        os.system("tar xvzf tmp.tgz && rm tmp.tgz")
+        print("done")
+        assert os.path.exists(images_name)
+
+    if not os.path.exists(attrs_name):
+        print("attributes not found, downloading...")
+        os.system("wget http://www.cs.columbia.edu/CAVE/databases/pubfig/download/%s"%attrs_name)
+        print("done")
+
+    #read attrs
+    df_attrs = pd.read_csv("lfw_attributes.txt",sep='\t',skiprows=1,) 
+    df_attrs = pd.DataFrame(df_attrs.iloc[:,:-1].values, columns = df_attrs.columns[1:])
+
+
+    #read photos
+    photo_ids = []
+    for dirpath, dirnames, filenames in os.walk(images_name):
+        for fname in filenames:
+            if fname.endswith(".jpg"):
+                fpath = os.path.join(dirpath,fname)
+                photo_id = fname[:-4].replace('_',' ').split()
+                person_id = ' '.join(photo_id[:-1])
+                photo_number = int(photo_id[-1])
+                photo_ids.append({'person':person_id,'imagenum':photo_number,'photo_path':fpath})
+
+    photo_ids = pd.DataFrame(photo_ids)
+
+    #mass-merge
+    #(photos now have same order as attributes)
+    df = pd.merge(df_attrs,photo_ids,on=('person','imagenum'))
+
+    assert len(df)==len(df_attrs),"lost some data when merging dataframes"
+
+    #image preprocessing
+    all_photos =df['photo_path'].apply(imread)\
+                                .apply(lambda img:img[dy:-dy,dx:-dx])\
+                                .apply(lambda img: imresize(img,[dimx,dimy]))
+
+    all_photos = np.stack(all_photos.values).astype('uint8')
+    all_attrs = df.drop(["photo_path","person","imagenum"],axis=1)
+    
+    return all_photos,all_attrs
+    
