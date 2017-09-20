f0a07566fdfd2a5ceb2b9ec4b39ae519f8a74739
Just Heuristic <justheuristic@gmail.com>
2017-09-20 19:46:34 +0300

    week2

---
+++ week2/cifar.py
@@ -0,0 +1,52 @@
+"""I load some cifar"""
+
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import urllib2
+import urllib
+def unpickle(file):
+    import cPickle
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo)
+    fo.close()
+    return dict
+
+
+import os
+def download_cifar10(path,
+                     url='https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+                     tarname='cifar-10-python.tar.gz'):
+    import tarfile
+    if not os.path.exists(path):
+        os.mkdir(path)
+    
+        
+
+    urllib.urlretrieve(url, os.path.join(path,tarname))
+    tfile = tarfile.open(os.path.join(path,tarname))
+    tfile.extractall(path=path)
+    
+
+def load_cifar10(data_path=".",test_size=0.2,random_state=1337):
+    
+    test_path = os.path.join(data_path,"cifar-10-batches-py/test_batch")
+    train_paths = [os.path.join(data_path,"cifar-10-batches-py/data_batch_%i"%i) for i in range(1,6)]
+    
+    if not os.path.exists(test_path) or not all(list(map(os.path.exists, train_paths))):
+        print "Dataset not found. Downloading..."
+        download_cifar10(data_path)
+
+    train_batches = list(map(unpickle,train_paths))
+    test_batch = unpickle(test_path)
+
+    X = np.concatenate([batch["data"] for batch in train_batches]).reshape([-1,3,32,32]).astype('float32')/255
+    y = np.concatenate([batch["labels"] for batch in train_batches]).astype('int32')
+    X_train,X_val,y_train,y_val = train_test_split(X,y,
+                                                   test_size=test_size,
+                                                   random_state=random_state)
+    
+    X_test = test_batch["data"].reshape([-1,3,32,32]).astype('float32')/255
+    y_test = np.array(test_batch["labels"]).astype('int32')
+    
+    return X_train,y_train,X_val,y_val,X_test,y_test
+    
